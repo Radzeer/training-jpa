2b6bb529001a32bab9212a0375d273254cfa5223
Radzeer <hollo.attila.82@gmail.com>
2022-10-03 11:41:12 +0200

    jpa

---
+++ src/main/java/training/employees/employees/entity/Employee.java
@@ -0,0 +1,23 @@
+package training.employees.employees.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+//@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int yearOfBirth;
+}
---
+++ src/main/java/training/employees/employees/repository/EmployeesRepository.java
@@ -0,0 +1,8 @@
+package training.employees.employees.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import training.employees.employees.entity.Employee;
+
+public interface EmployeesRepository extends JpaRepository<Employee,Long> {
+}
