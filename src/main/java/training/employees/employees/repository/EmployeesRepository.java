920eb709a4660063e3622dd41e74b19fd2e44a63
Radzeer <hollo.attila.82@gmail.com>
2022-10-03 14:08:55 +0200

    CQRS ( select gets DTO, not entity)

--- src/main/java/training/employees/employees/repository/EmployeesRepository.java
+++ src/main/java/training/employees/employees/repository/EmployeesRepository.java
@@ -3,6 +3,7 @@ package training.employees.employees.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import training.employees.employees.dto.EmployeeDto;
 import training.employees.employees.entity.Employee;
 
 import java.util.List;
@@ -13,4 +14,8 @@ public interface EmployeesRepository extends JpaRepository<Employee,Long> {
 
     //INFO: method név alapján elkészítia  qery-t
     List<Employee> findAllByNameLike(String prefix);
+    @Query("select new training.employees.employees.dto.EmployeeDto(e.id,e.name) from Employee e")
+    List<EmployeeDto> findEmployeeDtos();
+    @Query("select new training.employees.employees.dto.EmployeeDto(e.id,e.name) from Employee e where e.name like :prefix")
+    List<EmployeeDto> findEmployeeDtosByNameLike(String prefix);
 }
--- src/main/java/training/employees/employees/service/EmployeesService.java
+++ src/main/java/training/employees/employees/service/EmployeesService.java
@@ -24,10 +24,10 @@ public class EmployeesService {
 
     public List<EmployeeDto> listEmployees(Optional<String> prefix) {
         if (prefix.isEmpty()) {
-            return employeeMapper.toDto(repository.findAll());
+            return repository.findEmployeeDtos();
         }
         else {
-            return employeeMapper.toDto(repository.findAllByNameLike(prefix.get().toLowerCase() + "%"));
+            return repository.findEmployeeDtosByNameLike(prefix.get().toLowerCase() + "%");
         }
     }
 
